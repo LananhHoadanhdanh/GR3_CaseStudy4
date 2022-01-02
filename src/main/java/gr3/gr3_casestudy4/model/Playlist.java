a857c2a580d4495b0b35d8b5631d4d1a6daf6fd2
Phạm Thị Lan Anh <90228386+LananhHoadanhdanh@users.noreply.github.com>
2022-01-02 17:10:26 +0700

    tạo bảng playlist

---
+++ src/main/java/gr3/gr3_casestudy4/model/Playlist.java
@@ -0,0 +1,73 @@
+package gr3.gr3_casestudy4.model;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+public class Playlist {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    private String name;
+
+    @ManyToOne
+    private User user;
+
+    private int status;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "playlist_song",
+            joinColumns = {@JoinColumn(name = "playlist_id")},
+            inverseJoinColumns = {@JoinColumn(name = "song_id")})
+    private Set<Song> songs;
+
+    public Playlist() {
+    }
+
+    public Playlist(String name, User user, Set<Song> songs) {
+        this.name = name;
+        this.user = user;
+        this.songs = songs;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public Set<Song> getSongs() {
+        return songs;
+    }
+
+    public void setSongs(Set<Song> songs) {
+        this.songs = songs;
+    }
+}
