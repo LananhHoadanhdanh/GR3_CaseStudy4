a384e45ce36d9b61a57da59812dc16f44e332b89
Phạm Thị Lan Anh <90228386+LananhHoadanhdanh@users.noreply.github.com>
2022-01-02 16:39:47 +0700

    ok

---
+++ src/main/java/gr3/gr3_casestudy4/Gr3CaseStudy4Application.java
@@ -0,0 +1,13 @@
+package gr3.gr3_casestudy4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gr3CaseStudy4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gr3CaseStudy4Application.class, args);
+    }
+
+}
---
+++ src/test/java/gr3/gr3_casestudy4/Gr3CaseStudy4ApplicationTests.java
@@ -0,0 +1,13 @@
+package gr3.gr3_casestudy4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gr3CaseStudy4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
